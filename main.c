e62de316f9fd3be8c36f03a3d3a04f2a3f4c6da4
yame <ya_me_game@outlok.de>
2023-01-28 08:54:25 +0100

    awd

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
-#include "stdio.h"
+#Include "stdio.h"
 #include "stdint.h"
 #include "string.h"
 #include "stdlib.h"
 
 // Regex Expression Format =  Object1 Logic&Control Object2 Logic&Control Object3 ...
-                                  
+
 #define MAX_ARGUMENT_LENGTH 512
 #define MAX_ARGUMENTS       26
 #define MAX_OP_CODES        255
 #define MAX_WORD_LENGTH     256
 
-//Todo : Try Out text++ to search throught text;
+
+//Todo : Try Out text++ to search throught text
+//Todo : Add Default Groups And Group Tests
 //Todo : Build Execution Engine
+
+//ASCII Letters Are form 65 to 90 For Capital Letters and 97 to 122 for lowercase letters
 
 typedef enum
 {
@@ -41,7 +45,7 @@ typedef enum
  XOR,
 }Regex_Logic;
 
-typedef struct 
+typedef struct
 {
  Regex_Object    object;
  Regex_OpCode    op_code[MAX_OP_CODES];
@@ -54,21 +58,21 @@ typedef struct
 
 typedef uint8_t (*Regex_Action)(uint64_t trigger, char* text);
 
-uint8_t TestAction(uint64_t trigger , char* text);
+int8_t TestAction(uint64_t trigger , char* text);
 
-uint8_t Regex_AddOpCode(Regex_Operation *operation, Regex_OpCode op_code, char* argument);
+int8_t Regex_AddOpCode(Regex_Operation *operation, Regex_OpCode op_code, char* argument);
 
-uint8_t Regex_ExecuteOperations(char* text, Regex_Operation* operations, uint16_t op_count , Regex_Action action);
+int8_t Regex_ExecuteOperations(char* text, Regex_Operation* operations, uint16_t op_count , Regex_Action action);
 
-int64_t Regex_JumpToLetter(char* text, uint64_t current_pos);
+int32_t Regex_JumpToLetter(char* text, uint64_t current_pos);
 
-int16_t Regex_IsStrictWord(char* text, uint64_t pos);
+int8_t Regex_IsStrictWord(char* text, uint64_t pos);
 
-uint64_t Regex_GetObject(Regex_Object object, uint64_t current_pos, char* text);
+int32_t Regex_GetObject(Regex_Object object, uint64_t current_pos, char* text);
 
-int8_t Regex_GetObjectStrictWord(uint64_t pos,char* text,char word[MAX_WORD_LENGTH]);
+int32_t Regex_GetObjectStrictWord(uint64_t pos,char* text,char word[MAX_WORD_LENGTH]);
 
-int8_t Regex_GetObjectStrictChar(uint64_t pos,char* text,char* strict_char);
+int32_t Regex_GetObjectStrictChar(uint64_t pos,char* text,char* strict_char);
 
 int main()
 {
@@ -77,29 +81,20 @@ int main()
  operation.logic=NOT;
  operation.object=STRICT_WORD;
  Regex_AddOpCode(&operation,EQUAL,"Hallo");
-  
+
  char* test="Bu!lu % Blib Blabber";
 
  Regex_GetObject(STRICT_WORD,0,test);
  Regex_GetObject(STRICT_CHAR,0,test);
  Regex_GetObject(CHAR,0,test);
+ Regex_GetObject(STRICT_WORD,0,test);
+
 
  printf(" %s \n ", operation.argument[0]);
 
  return 0;
 }
 
-uint8_t Regex_AddOpCode(Regex_Operation *operation, Regex_OpCode op_code, char* argument)
-{
- if(operation->operation_count>MAX_OP_CODES){ printf(" To Many OpCodes Add Failed\n "); return 0; }
-
- operation->op_code[operation->operation_count]=op_code;
- strcpy(operation->argument[operation->operation_count],argument);
- 
- operation->operation_count++;
-
- return 1;
-}
 
 uint8_t Regex_ExecuteOperations(char* text, Regex_Operation* operations, uint16_t op_count , Regex_Action action)
 {
@@ -107,7 +102,104 @@ uint8_t Regex_ExecuteOperations(char* text, Regex_Operation* operations, uint16_
 }
 
 
-//ASCII Letters Are form 65 to 90 For Capital Letters and 97 to 122 for lowercase letters
+// ----------------------------------------------------------------- Word Functions Start
+
+int64_t Regex_GetObjectStrictWord(uint64_t pos,char* text,char word[MAX_WORD_LENGTH])
+{
+  int16_t is_word=0;
+  int64_t jump=0;
+
+  while(1){
+    jump=Regex_JumpToLetter(text,pos);
+
+    if(jump==-1){ printf(" Couldnt Find Object"); return 0; }
+
+    pos+=jump;
+    jump=0;
+
+    is_word=Regex_IsStrictWord(text,pos);
+
+    if(is_word!=-1){
+      strncpy(word,&text[pos],is_word);
+      word[is_word+1]='\0';
+      printf(" Found Word %s \n ", word);
+      break;
+    }
+
+    while(text[pos]!=' '){
+      pos++;
+
+      if(text[pos]=='\0'){
+        printf(" Couldnt Find Object"); return 0;
+      }
+    }
+
+  }
+}
+
+int16_t Regex_IsStrictWord(char* text, uint64_t pos)
+{
+  uint16_t length=0;
+
+  while( (text[pos]>64 && text[pos]<91) || (text[pos]>96 && text[pos]<128)  ){
+    printf(" Found Letter %c at pos %d \n ", text[pos],pos);
+    length++;
+    pos++;
+  }
+
+  if(text[pos]=='\0' || text[pos]==' '){  return length; }
+
+  printf(" Found SpecialChar %c at pos %d \n ", text[pos],pos);
+
+  return -1;
+}
+
+
+// ----------------------------------------------------------------- Word Functions End
+
+
+// ----------------------------------------------------------------- Char Functions Start
+
+int8_t Regex_GetObjectStrictChar(uint64_t pos,char* text,char* strict_char)
+{
+
+  while(text[pos]!='\0')
+    {
+
+      if( (text[pos]>64 && text[pos]<91) || (text[pos]>96 && text[pos]<128) ){
+        printf(" Strict Character %c", text[pos]);
+        strict_char=text[pos];
+        return pos;
+      }
+
+      pos++;
+    }
+
+  printf(" No Object Found \n ");
+  return -1;
+}
+
+// ----------------------------------------------------------------- Char Functions End
+
+
+// ----------------------------------------------------------------- Op Code Functions Start
+
+uint8_t Regex_AddOpCode(Regex_Operation *operation, Regex_OpCode op_code, char* argument)
+{
+ if(operation->operation_count>MAX_OP_CODES){ printf(" To Many OpCodes Add Failed\n "); return 0; }
+
+ operation->op_code[operation->operation_count]=op_code;
+ strcpy(operation->argument[operation->operation_count],argument);
+
+ operation->operation_count++;
+
+ return 1;
+}
+
+// ----------------------------------------------------------------- Op Code Functions End
+
+
+
 
 uint64_t Regex_GetObject(Regex_Object object, uint64_t current_pos, char* text)
 {
@@ -118,11 +210,11 @@ uint64_t Regex_GetObject(Regex_Object object, uint64_t current_pos, char* text)
  uint64_t pos=current_pos;
 
  switch(object)
- { 
+ {
   case STRICT_WORD: Regex_GetObjectStrictWord(pos,text,word);
                     break;
 
-  case STRICT_CHAR: Regex_GetObjectStrictChar(pos,text,&strict_char); 
+  case STRICT_CHAR: Regex_GetObjectStrictChar(pos,text,&strict_char);
 
   case CHAR:if(text[current_pos]=='\0'){ printf(" No Futher Characters \n "); return 0; }
             character=text[current_pos];
@@ -135,82 +227,17 @@ uint64_t Regex_GetObject(Regex_Object object, uint64_t current_pos, char* text)
 
 }
 
-int8_t Regex_GetObjectStrictChar(uint64_t pos,char* text,char* strict_char)
-{
 
- while(text[pos]!='\0')
- {
 
-  if( (text[pos]>64 && text[pos]<91) || (text[pos]>96 && text[pos]<128) ){ 
-   printf(" Strict Character %c", text[pos]);
-   strict_char=text[pos]; 
-   return pos; 
-  }
 
-  pos++;
- }
- 
- printf(" No Object Found \n ");
- return -1;
-}
-
-int8_t Regex_GetObjectStrictWord(uint64_t pos,char* text,char word[MAX_WORD_LENGTH])
-{
- int16_t is_word=0;  
- int64_t jump=0;
-
- while(1){
-  jump=Regex_JumpToLetter(text,pos);
- 
-  if(jump==-1){ printf(" Couldnt Find Object"); return 0; }
- 
-  pos+=jump;
-  jump=0;
-  
-  is_word=Regex_IsStrictWord(text,pos);
-             
-  if(is_word!=-1){ 
-   strncpy(word,&text[pos],is_word);
-   word[is_word+1]='\0';
-   printf(" Found Word %s \n ", word);
-   break;
-  }
-             
-  while(text[pos]!=' '){ 
-   pos++; 
-
-   if(text[pos]=='\0'){
-    printf(" Couldnt Find Object"); return 0; 
-   }
-  }
-
- }
-}
-
-int16_t Regex_IsStrictWord(char* text, uint64_t pos)
-{
- uint16_t length=0;
- 
- while( (text[pos]>64 && text[pos]<91) || (text[pos]>96 && text[pos]<128)  ){ 
-   printf(" Found Letter %c at pos %d \n ", text[pos],pos);
-   length++;
-   pos++;
- }
- 
- if(text[pos]=='\0' || text[pos]==' '){  return length; } 
-
- printf(" Found SpecialChar %c at pos %d \n ", text[pos],pos);
-
- return -1;
-}
-
+// ----------------------------------------------------------------- Utils
 
 int64_t Regex_JumpToLetter(char* text, uint64_t pos)
 {
 
  uint64_t length=0;
 
- while(! ( (text[pos]>64 && text[pos]<91) || (text[pos]>96 && text[pos]<128) ) ){ 
+ while(! ( (text[pos]>64 && text[pos]<91) || (text[pos]>96 && text[pos]<128) ) ){
    if(text[pos]=='\0'){ return -1; }
    pos++;
    length++;
